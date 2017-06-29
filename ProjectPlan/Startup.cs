cef4743362ddf4ac8c2ff09888883bc04408aea4
scip4 <scip31@gmail.com>
2017-06-29 12:59:08 -0400

    Add project files.

---
+++ ProjectPlan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectPlan.Startup))]
+namespace ProjectPlan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
